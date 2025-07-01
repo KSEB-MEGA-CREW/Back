e904818d57798b773fe6327bd909df4773e9cd24
ChoiMinSang <min393917@gmail.com>
2025-07-01 16:33:57 +0900

    initial commit

---
+++ src/main/java/org/example/mega_crew/MegaCrewApplication.java
@@ -0,0 +1,13 @@
+package org.example.mega_crew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaCrewApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MegaCrewApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/mega_crew/MegaCrewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mega_crew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaCrewApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
