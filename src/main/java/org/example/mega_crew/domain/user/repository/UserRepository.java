ef7d37f0eb1e9716fb7555d5043a752cd235a3d8
ChoiMinSang <min393917@gmail.com>
2025-07-08 16:58:39 +0900

    Refactor DB table and JPA Query

--- src/main/java/org/example/mega_crew/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/mega_crew/domain/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package org.example.mega_crew.domain.user.repository;
 
 
 import org.example.mega_crew.domain.user.entity.AuthProvider;
+import org.example.mega_crew.domain.user.entity.HearingStatus;
 import org.example.mega_crew.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     Optional<User> findByAuthProviderAndProviderId(AuthProvider authProvider, String providerId);
+
+    Optional<User> findByUsername(String username);
+    List<User> findByHearingStatus(HearingStatus hearingStatus);
 }
