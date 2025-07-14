f8044b4bd0bc8992db7a34e6ffbf4a2ee1b830cb
ChoiMinSang <min393917@gmail.com>
2025-07-14 17:39:40 +0900

    feat connect-unity practice

---
+++ src/main/java/org/example/mega_crew/global/websocket/handler/MyWebSocketHandler.java
@@ -0,0 +1,18 @@
+package org.example.mega_crew.global.websocket.handler;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+public class MyWebSocketHandler extends TextWebSocketHandler {
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+    // client로부터 메시지 수신
+    System.out.println("Received: " + message.getPayload());
+
+    // 메시지 응답
+    session.sendMessage(new TextMessage("Echo: "+ message.getPayload()));
+  }
+}
