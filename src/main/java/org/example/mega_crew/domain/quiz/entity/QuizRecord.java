4c78f987c478d876f3e4c8418d3de8db4f19bf0c
ChoiMinSang <min393917@gmail.com>
2025-07-22 16:58:55 +0900

    add QuizRecord

---
+++ src/main/java/org/example/mega_crew/domain/quiz/entity/QuizRecord.java
@@ -0,0 +1,29 @@
+package org.example.mega_crew.domain.quiz.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.mega_crew.domain.user.entity.User;
+import org.example.mega_crew.global.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+public class QuizRecord extends BaseEntity {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   private int correctCount;
+
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "user_id")
+   private User user;
+
+   public QuizRecord() {}
+
+   public QuizRecord(int correctCount, User user) {
+      this.correctCount = correctCount;
+      this.user = user;
+   }
+}
