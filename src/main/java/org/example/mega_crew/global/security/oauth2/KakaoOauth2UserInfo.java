081cc63f90560b37200f75ce50afadb541f9b202
ChoiMinSang <min393917@gmail.com>
2025-07-30 14:24:07 +0900

    feat kakao yml file

---
+++ src/main/java/org/example/mega_crew/global/security/oauth2/KakaoOauth2UserInfo.java
@@ -0,0 +1,16 @@
+package org.example.mega_crew.global.security.oauth2;
+
+import java.util.Map;
+
+public class KakaoOauth2UserInfo extends OAuth2UserInfo{
+   public KakaoOauth2UserInfo(Map<String, Object> attributes){ super(attributes); }
+
+   @Override
+   public String getId() { return ""; }
+
+   @Override
+   public String getName() { return ""; }
+
+   @Override
+   public String getEmail() { return ""; }
+}
